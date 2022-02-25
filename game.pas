d6117a1ccedd92b77d83458cc60df374abd33675
ErmD <100240829+ErmD@users.noreply.github.com>
2022-02-25 14:25:54 +0300

    Дописал время раунда и очки

--- game.pas
+++ game.pas
@@ -1,136 +1,141 @@
-﻿uses GraphWPF, WPFObjects;
+﻿uses GraphWPF, WPFObjects, Timers;
 
-var ballRadius := 20;
+var ballRadius := 30;
+var (x,y) := (ballRadius,ballRadius);
+var Ball := new CircleWPF(ballRadius,ballRadius,ballRadius,gcolor.Fromrgb(255,0,0));
+var (ml,mr,mt,mb) :=(false,false,false,false);
 
-var Ball := new CircleWPF(ballRadius,ballRadius,ballRadius,gcolor.FromRgb(255,0,0));
-var Enemy := new CircleWPF(810-ballRadius,610-ballRadius,ballRadius,gcolor.FromRgb(0,0,255));
-var (ml,mr,mt,mb) := (false,false,false,false);
-var (el,er,et,eb) := (false,false,false,false);
+var ballRadius1 := 20;
+var (x1,y1) := (810-ballRadius1,610-ballRadius1);
+var Enemy := new CircleWPF(810-ballRadius1,610-ballRadius1,ballRadius1,gcolor.Fromrgb(0,0,255));
+var (el,er,et,eb) :=(false,false,false,false);
 
+//Настройки
 procedure setup();
-begin
-  Window.SetSize(800,600);
-  Window.Caption := 'Игра из лужи';
-  Window.IsFixedSize := true;
-  //Window.CenterOnScreen();
-   
-end;
-
-procedure KeyDown(k: Key);
-begin
-  case k of
-    Key.W: mt := true;
-    Key.S: mb := true; 
-    Key.A: ml := true;
-    Key.D: mr := true;
-    
-    Key.Up: et := true;
-    Key.Down: eb := true; 
-    Key.Left: el := true;
-    Key.Right: er := true;
-  end;
-end;
-
-procedure KeyUp(k: Key);
-begin
-  case k of
-    Key.W: mt := false;
-    Key.S: mb := false; 
-    Key.A: ml := false;
-    Key.D: mr := false;
-    
-    Key.Up: et := false;
-    Key.Down: eb := false; 
-    Key.Left: el := false;
-    Key.Right: er := false;
-  end;
-end;
-
-{
-procedure MouseDown(x, y: real; mousebutton: integer);
-begin
-  if (mousebutton = 1) then 
   begin
-    var c := new CircleWPF(x,y,20,gcolor.FromRgb(0,0,255));
+    Window.SetSize(800,600);
+    Window.Caption := 'Игра из палок';
+    Window.IsFixedSize := true;
+    Window.CenterOnScreen();
+  end;
+
+//Клавиши
+procedure keyDown(k: Key);
+begin
+  case k of
+    Key.W: mt :=true;
+    Key.S: mb :=true;
+    Key.A: ml :=true;
+    Key.D: mr :=true;
+    
+    Key.Up: et :=true;
+    Key.Down: eb :=true;
+    Key.Left: el :=true;
+    Key.Right: er :=true;
   end;
 end;
-}
 
+procedure keyUp(k: Key);
 begin
+  case k of
+    Key.W: mt :=false;
+    Key.S: mb :=false;
+    Key.A: ml :=false;
+    Key.D: mr :=false;
+    
+    Key.Up: et :=false;
+    Key.Down: eb :=false;
+    Key.Left: el :=false;
+    Key.Right: er :=false;
+  end;
+end;
+
+procedure TimerBall;
+begin  
+  var Going:= new TextWPF(343,270,40,'Догнал!', colors.blue);
+  Going.BackgroundColor := colors.Black;
+  Going.Color := colors.White;
+  Going.FontSize:= 40;
+  sleep(1000);
+  Going.Destroy;
+end;
+
+//Основа
+begin
+  
   setup();
-  //OnMouseDown := MouseDown;
+
   OnKeyDown := KeyDown;
   OnKeyUp := KeyUp;
   
+  Var t := new Timer(100,TimerBall);
+  
   var gameState := true;
-  
-  var (x,y) := (ballRadius,ballRadius);
-  var (x1,y1) := (810-ballRadius,610-ballRadius);
-  
-  while gameState do 
-  begin
-    
-    if ObjectsIntersect(Ball,Enemy) then 
+  while gameState do
     begin
-      Ball.Left := 0;
-      Ball.Top := 0;
-      (x,y) := (ballRadius,ballRadius);
+      if ObjectsIntersect(Ball,Enemy) then 
+        begin
+          
+          t.Start;
+          Sleep(100);
+          t.Interval := 1500;
+          Sleep(3000);
+          t.Stop;
+
+          Ball.Left := 0;
+          Ball.Top := 0;
+          (x,y) := (ballRadius,ballRadius);
+          
+          Enemy.Left := 810-2*ballRadius1;
+          Enemy.Top := 610-2*ballRadius1;
+          (x1,y1) := (810-ballRadius1,610-ballRadius1);
+          
+        end;
       
-      Enemy.Left := 810-2*ballRadius;
-      Enemy.Top := 610-2*ballRadius;
-      (x1,y1) := (810-ballRadius,610-ballRadius);
-      
+      if ((ml)and(x>ballRadius)) then 
+        begin
+          Ball.Left -= 1;
+          x-=1;
+        end;
+      if ((mr)and(x<810-ballRadius)) then 
+        begin
+          Ball.Left += 1;
+          x+=1;
+        end;
+      if ((mt)and(y>ballRadius)) then
+        begin
+          Ball.Top -= 1;
+          y-=1;
+        end;
+      if ((mb)and(y<610-ballRadius)) then 
+        begin
+          Ball.Top += 1;
+          y+=1;
+        end;
+          
+        {----------------------------------------}
+        
+      if ((el)and(x1>ballRadius1)) then 
+        begin
+          Enemy.Left -= 1;
+          x1-=1;
+        end;
+      if ((er)and(x1<810-ballRadius1)) then 
+        begin
+          Enemy.Left += 1;
+          x1+=1;
+        end;
+      if ((et)and(y1>ballRadius1)) then
+        begin
+          Enemy.Top -= 1;
+          y1-=1;
+        end;
+      if ((eb)and(y1<610-ballRadius1)) then 
+        begin
+          Enemy.Top += 1;
+          y1+=1;
+        end;
+    sleep(2);
     end;
-    
-    
-    if ((ml) and (x>ballRadius)) then 
-      begin
-        Ball.Left -= 1;
-        x -= 1;
-      end;
-    if ((mr) and (x<810-ballRadius)) then 
-      begin
-        Ball.Left += 1;
-        x += 1;
-      end;
-
-    if ((mt) and (y>ballRadius)) then 
-      begin
-        Ball.Top -= 1;
-        y -= 1;
-      end;
-
-     if ((mb) and (y<610-ballRadius)) then 
-      begin
-        Ball.Top += 1;
-        y += 1;
-      end;
-     {----------------------------------------} 
-    if ((el) and (x1>ballRadius)) then 
-      begin
-        Enemy.Left -= 1;
-        x1 -= 1;
-      end;
-    if ((er) and (x1<810-ballRadius)) then 
-      begin
-        Enemy.Left += 1;
-        x1 += 1;
-      end;
-
-    if ((et) and (y1>ballRadius)) then 
-      begin
-        Enemy.Top -= 1;
-        y1 -= 1;
-      end;
-
-     if ((eb) and (y1<610-ballRadius)) then 
-      begin
-        Enemy.Top += 1;
-        y1 += 1;
-      end;
-    
-    sleep(1);
-  end;
-  
   
 end.
